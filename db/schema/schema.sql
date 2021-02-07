ff21b46b7bded2b337550b6a296a4ab3e313671a
loloffs <lukeoloffs@gmail.com>
2021-02-07 22:25:49 +0000

    Altered db

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -6,14 +6,14 @@ DROP TABLE IF EXISTS favourites CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(255) NOT NULL,
-  email VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL
   -- updated_at DATE NOT NULL, -- optional
   -- created_at DATE NOT NULL, -- optional
   -- password VARCHAR(255) NOT NULL -- Do we even need a password?
 );
 
 
-CREATE TYPE condition_option as enum('Like new', 'Good', 'Average', 'Used');
+-- CREATE TYPE condition_option as enum('Like new', 'Good', 'Average', 'Used');
 
 CREATE TABLE listings (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -22,15 +22,15 @@ CREATE TABLE listings (
   description TEXT,
   price FLOAT  NOT NULL DEFAULT 0,
   author VARCHAR(100) NOT NULL,
-  condition condition_option VARCHAR(100) NOT NULL, -- consider: ENUM ('new', 'used', 'lightly used') Ask mentor if I did this right
+  condition VARCHAR(100) NOT NULL, -- consider: ENUM ('new', 'used', 'lightly used') Ask mentor if I did this right
   picture_url VARCHAR(255),
-  isSold BOOLEAN NOT NULL DEFAULT FALSE,
+  isSold BOOLEAN NOT NULL DEFAULT FALSE
   -- updated_at DATE NOT NULL,
   -- created_at DATE NOT NULL
 );
 
 CREATE TABLE favourites (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  listing_id INTEGER REFERENCES listings(id) ON DELETE CASCADE,
+  listing_id INTEGER REFERENCES listings(id) ON DELETE CASCADE
   -- created_at DATE NOT NULL
 );
