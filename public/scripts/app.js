62a0b8a0736af0e042a4407006389bf59f61824a
vagrant <mveri.bruno@gmail.com>
2021-02-09 21:18:45 +0000

    Implementing login

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,12 +7,22 @@
 
 
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
+console.log("TEST");
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+  $("#login-button").click(() => {
+    console.log("log and click");
+     $.ajax({
+    method: "POST",
+    url: "/login"
+  }).done(() => {
+    console.log("login complete");
   });;
+  });
 });
