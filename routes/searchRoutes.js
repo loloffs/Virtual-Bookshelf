ab470de92f13ff1d76743c7afb0c4c4d12038a17
JSmith604 <jacquelinesmith190@gmail.com>
2021-02-10 23:34:21 +0000

    Get rid of some commented out code

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -14,13 +14,10 @@ module.exports = (pool) => {
 
     //Make sure the form method and form action in the html document match those on the search routes, i.e. GET and /main //make sure index form references minPrice, maxPrice, title, autor //form values within the options tag must match up with search routes //implement form validation on the front end, i.e. please enter a number
 
-    //Connect the drop down menu to the backend
-
-    //Use select element in html (as opposed to a navigational drop down)?
 
     //Make a second drop down for order by (form drop down box)?
 
-    //Divide this function up and make separate search routes?
+
     //We should have two optional drop down menu's (form data drop down menu's as linked in discord chat) for the search type and the order in the same form to get the query paramaters working
     let orderBy = "price";
 
@@ -70,7 +67,7 @@ module.exports = (pool) => {
      };
      res.render("index", templateVars);
   })
-  return app;  //What is this doing? Ask mentor
+  return app;
 };
 
 
