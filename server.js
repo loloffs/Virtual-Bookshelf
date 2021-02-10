5377fbf9b072b6d7beac9785d93c844e1d2f13c3
JSmith604 <jacquelinesmith190@gmail.com>
2021-02-10 02:11:58 +0000

    Commit server.js changes to merge

--- server.js
+++ server.js
@@ -35,11 +35,12 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
-
+const searchRoutes = require("./routes/searchRoutes");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/search", searchRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
