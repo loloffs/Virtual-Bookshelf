98ad5446925eb09fa040af6be9a28d170d83b64d
loloffs <lukeoloffs@gmail.com>
2021-02-10 17:26:53 +0000

    Fixed syntax issues

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
-const searchRoutes = require("./routes/searchRoutes");
+// const searchRoutes = require("./routes/searchRoutes");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
