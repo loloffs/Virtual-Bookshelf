112b5663f2379e264f89a8733c5c3657ae6db5e3
JSmith604 <jacquelinesmith190@gmail.com>
2021-02-11 22:20:13 +0000

    Start fixing heart

--- public/scripts/heart.js
+++ public/scripts/heart.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   $("#favourite-button").on('click'), function(event) {
     let heartIcon = $(event.target).find('.fas-fa-heart')
+    console.log(heartIcon);
      heartIcon.toggleClass('activeHeartIcon');
    }
 });
